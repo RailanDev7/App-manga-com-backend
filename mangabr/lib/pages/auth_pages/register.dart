30477dcdccc85907e248b054111aad861bd757e3
Railan <railandejesusdossantos@gmail.com>
2025-12-13 23:20:53 -0300

    codigo melhorado

--- mangabr/lib/pages/auth_pages/register.dart
+++ mangabr/lib/pages/auth_pages/register.dart
@@ -6,48 +6,70 @@ class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsetsGeometry.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Image.asset('assets/logo.png'),
-            SizedBox(height: 15),
-            //barra para escrever os dados
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Digite seu Email',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 15),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                hintText: 'Digite uma senha',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 25),
-            //botao register
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.deepPurple,
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                  vertical: 15,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Image.asset('assets/logo.png'),
+              SizedBox(height: 15),
+              //barra para escrever os dados
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                  hintText: 'Seu nome',
+                  border: OutlineInputBorder(),
                 ),
               ),
-              child: Text('Criar conta'),
-            ),
-          ],
+              SizedBox(height: 15),
+
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Digite seu Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 15),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  hintText: 'Digite uma senha',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 25),
+              //botao register
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 15,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Text('Criar conta', style: TextStyle(fontSize: 24)),
+              ),
+              SizedBox(height: 30),
+              Text('Já tem uma conta?', style: TextStyle(fontSize: 20)),
+              GestureDetector(
+                onTap: () {
+                  print('clicou');
+                },
+                child: Text(
+                  'Fazer login!',
+                  style: TextStyle(color: Colors.deepPurple, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
